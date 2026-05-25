76bb1af4bb86fbeae3d7c5633031f74aef325e4b
GitRit7 <ritamdey45@gmail.com>
2026-05-25 13:39:13 +0530

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String args[]){
+        System.out.println("HANGMAN-guess word, 6 turns");
+        String word = "mountain";
+        
+        Scanner sc = new Scanner(System.in);
+
+        String guess;
+
+        int l = word.length();
+
+        for(int i = 1; i <= 6; i++){
+            System.out.println("Enter guess below:-");
+            guess = sc.nextLine();
+            try{
+                if(guess.equals(word)){
+                    System.out.println("correct, you win");
+                    break;
+                }else{
+                    for(int j = 0; j < l; j++){
+                        if(word.charAt(j) == guess.charAt(j)){
+                            System.out.print(guess.charAt(j));
+                        }else{
+                            System.out.print("_");
+                        }
+                    }
+                    System.out.println(" ");
+                    System.out.println("wrong, "+(6-i)+" turns left.");
+                    if((6-i) == 0){
+                        System.out.println("you lose");
+                    }
+                }
+            }catch(StringIndexOutOfBoundsException e){
+                System.out.println("invalid length");
+                i--;
+                
+            }
+        }
+        sc.close();
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+HANGMAN
+
+Get 6 chances to guess the word.
+
+-length of word to be guessed is given.
+-type word, if you are correct you win.
+-if not, you will be given the letters that were right at the right positions.
+-checks if you gave a guess with incorrect length.
+
+how to run
+-clone repo
+-open in any IDE then run
+-or open in terminal:-
+    (currect directory should be project folder location)
+
+    javac Main.java
+    java Main
+    
+    (for this, java path should be created already)
+
+future improvements:
+
+-use file handling to put word to guess in a different file
+-add GUI
+
+Author
+-GitRit7, college student.
